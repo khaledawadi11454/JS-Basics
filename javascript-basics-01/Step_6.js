b0db2dd605c8f648194907f07ae310bff081b41f
khaled <awadkhaled.webdev@gmail.com>
2022-12-07 13:47:28 +0300

    completed Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,14 @@
+/*let numb1 =parseInt(prompt("insert numb1 :"));
+let numb2 =parseInt(prompt("insert numb2 :"));
+let divide = numb1 / numb2;
+const rd=numb1 % numb2
+alert("the remainder of the division: " + rd);*/
+alert("Please Fill the form");
+function divide(){
+    var x=document.getElementById("first_number").value;
+    var y=document.getElementById("second_number").value;
+    var z= x%y;
+    alert("The remainder of this two numbers is  "+z);
+
+
+}
