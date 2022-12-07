1aadd0db8615b0fd94d7eda94acbf7aab58b9cab
khaled <awadkhaled.webdev@gmail.com>
2022-12-07 11:47:28 +0300

    completed Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+let name= prompt("insert your name :");
+let surname = prompt("insert your surname :");
+let city =prompt ("inster your city :");
+alert("name :" + name + "\n" + "surname :" + surname + "\n" + "city :" + city );
